9001e8225da8b62e7f63b5121f6e2af5400d084d
Darbnīca <hstrazds@gmail.com>
2014-11-06 13:16:45 +0200

    Initial Commit

---
+++ pirkumi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pirkumi
+//
+//  Created by Darbnīca on 06/11/14.
+//  Copyright (c) 2014 Skolnīca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
